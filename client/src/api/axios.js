919a5fa85601e0e37d4b63783ead9a6a49cf98b1
sabinseredan97 <114243765+sabinseredan97@users.noreply.github.com>
2023-07-10 16:28:27 +0300

    removed a function used for authorization before the first authorization updare

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001/api/";
 
-export function authorization() {
-  return axios.get("/users/auth").then((res) => res.data);
-}
-
 export function getUserData(username) {
   return axios.get(`profile/${username}`).then((res) => res.data);
 }
