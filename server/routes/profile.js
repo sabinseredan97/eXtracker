199afb82934ade2b89ae92f25044aec343f7da4a
sabinseredan97 <114243765+sabinseredan97@users.noreply.github.com>
2023-08-09 11:11:12 +0300

    small change at the profile controller

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getUserData } = require("../controllers/profileController");
+const {
+  getUserData,
+  getCategories,
+  addProduct,
+} = require("../controllers/profileController");
 const { authenticateToken } = require("../middlewares/tokenAuth");
-const { getCategories } = require("../controllers/profileController");
-const { addProduct } = require("../controllers/profileController");
 const { productValidator } = require("../middlewares/userDataValidation");
 
 router.get("/user-data/:username", authenticateToken, getUserData);
