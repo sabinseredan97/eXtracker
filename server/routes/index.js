e9261bbc68cd857a58312bc3a8115b3cdba4f9f1
sabinseredan97 <114243765+sabinseredan97@users.noreply.github.com>
2023-09-14 18:45:31 +0300

    update

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-router.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
