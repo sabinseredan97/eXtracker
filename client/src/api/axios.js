3c0c55d45ba32ac91d737ee54c1afe8e52f1a062
sabinseredan97 <114243765+sabinseredan97@users.noreply.github.com>
2023-10-31 18:08:56 +0200

    update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL =
-  "https://extracker-server-g9tff.ondigitalocean.app/api/";
+axios.defaults.baseURL = "https://extracker-server.onrender.com/api/";
+//"https://extracker-server-g9tff.ondigitalocean.app/api/";
 
 export function getUserData(username) {
   return axios.get(`users/data/${username}`).then((res) => res.data);
